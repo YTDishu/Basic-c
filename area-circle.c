3715b544ff2c5f41de2aedc1441a337ebff14151
Nootan <kumarinootan122@gmail.com>
2026-05-04 20:58:46 +0530

    Add area-circle.c and compiled binary
    Add a new C program area-circle.c that prompts for a circle radius, computes area using a float constant pie = 3.14, and prints the resulting area. The compiled binary a.exe is also added. Note: source file ends without a trailing newline.

---
+++ area-circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float pie =3.14;
+    float r, area ;
+
+    printf("enter the value of R   ");
+    scanf("%f",&r);
+
+    area = r*r *pie;
+
+    printf("%f" , area);
+    return 0;
+
+} 
